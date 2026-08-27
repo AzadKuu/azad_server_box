65c5d0fda2ef7edaa157c53a512fb859c26b6013
AZADKUU <309236878@qq.com>
2026-08-27 11:26:02 +0800

    文本修正

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,7 +2,7 @@
 // ignore_for_file: prefer_single_quotes
 
 abstract class BuildData {
-  static const String name = "ServerBox";
+  static const String name = "AzadHub";
   static const int build = 1491;
   static const int script = 73;
 }
